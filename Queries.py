f9f11eba220fa11ab967bc7749292c5da8128739
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-07-31 06:02:22 +0100

    Add files via upload

---
+++ Queries.py
@@ -0,0 +1,25 @@
+def GetUser(userName):
+    variables = {
+        'search' : userName
+    }
+    return variables
+
+def GetByTitle(type, title):
+    type = type.upper()
+    if type != 'ANIME' and type != 'MANGA':
+        return False
+    variables = {
+        'type' : type,
+        'search' : title
+    }
+    return variables
+
+def GetByID(type, id):
+    type = type.upper()
+    if type != 'ANIME' and type != 'MANGA':
+        return False
+    variables = {
+        'type' : type,
+        'id': id
+    }
+    return variables
