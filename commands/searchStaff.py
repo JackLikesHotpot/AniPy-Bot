15831abd7c115a868c1dc462a1fb30b80959baab
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-08-24 05:34:50 +0100

    added staff search function

---
+++ commands/searchStaff.py
@@ -0,0 +1,26 @@
+import discord
+from queries.staffQuery import searchStaff
+from queries.runQuery import run_query
+from variables.staffVar import GetByStaff
+
+def staffSearch(staffName):
+    query = searchStaff()
+    variables = GetByStaff(staffName)
+    if variables:
+        result = run_query(query, variables)
+        if not result:
+            return "There does not exist a person with the name {}.".format(staffName)
+
+        embed = discord.Embed(
+            colour=discord.Colour.dark_orange(),
+            title=result["data"]["Staff"]["name"]["full"],
+            url=result["data"]["Staff"]["siteUrl"]
+        )
+
+        characters = ""
+
+        for char in result["data"]["Staff"]["characters"]["edges"]:
+            characters += '[{}]({})'.format(char["node"]['name']['full'], char["node"]["siteUrl"]) + '\n\n'
+        embed.add_field(name="Character List", value=characters)
+        embed.set_thumbnail(url=result["data"]["Staff"]["image"]["large"])
+        return embed
