237f67de5c2ce6dfeb18f2e16082ee410d58d778
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-08-27 06:09:46 +0100

    Add files via upload

--- misc/clean.py
+++ misc/clean.py
@@ -2,3 +2,9 @@ def removeTags(text):
     import re
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
+
+
+def cutLength(text):
+    if len(text) > 900:
+        return text[:900] + "..."
+    return text
