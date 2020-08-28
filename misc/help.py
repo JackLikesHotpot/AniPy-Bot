68292d2261180d237eeb37b972c688d05aa24b82
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-08-28 04:18:34 +0100

    added all commands to the help embed

--- misc/help.py
+++ misc/help.py
@@ -10,4 +10,7 @@ def helpMessage():
     embed.add_field(name='!manga <title>', value="Search manga by title or ID.", inline=False)
     embed.add_field(name='!user <username>', value="Search up a user by their username.", inline=False)
     embed.add_field(name='!reverse <image link>', value="Search an anime by a link to an image.", inline=False)
+    embed.add_field(name='!studio <studio name>', value="Search a studio by their name.", inline=False)
+    embed.add_field(name='!staff <staff name>', value="Search an actor by their name.", inline=False)
+    embed.add_field(name='!char <character name>', value="Search a character by their name.", inline=False)
     return embed
