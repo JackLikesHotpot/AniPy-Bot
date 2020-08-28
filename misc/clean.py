d2c96ed17a1e30fa70d68286e98d00663a4fbd1e
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-08-28 04:18:14 +0100

    added a replaceNone function

--- misc/clean.py
+++ misc/clean.py
@@ -8,3 +8,9 @@ def cutLength(text):
     if len(text) > 900:
         return text[:900] + "..."
     return text
+
+
+def replaceNone(text):
+    if text is None:
+        return 'N/A'
+    return text
