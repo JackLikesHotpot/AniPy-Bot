1959d653bab807b3d174514bf2a1329e53154791
xjl98 <47116970+xjl98@users.noreply.github.com>
2020-08-28 04:48:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # From the Discord Developer Portal, copy your bot's token and paste it between the quotation marks.
+# You can also change the command prefix if needed. Default is !
 
 token=''
+prefix='!'
